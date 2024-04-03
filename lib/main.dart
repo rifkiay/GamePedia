4ab03bce1ee9f08fea39c1bfcb36b43230768770
unknown <rifkiainul422@gmail.com>
2024-04-03 16:56:15 +0700

    menambah file asset untuk image

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const MaterialApp(
+      title: 'GamePedia',
+      home: MainApp(),
+
+    )
+  );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatelessWidget{
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context){
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
         ),
+         title: const Text('Hello'),
       ),
+
+      body:  Container(
+          alignment: Alignment.center,
+          color: Colors.blue,
+          constraints: const BoxConstraints.expand(), // Menyesuaikan ukuran Container dengan ukuran child
+          child: Column(children: [
+            const Text('Rifki'),
+            Image.asset('images/image.jpeg')
+          ],)
+      ),
+      )
     );
   }
 }
+
+
