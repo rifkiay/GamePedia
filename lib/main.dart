47ba35b0c3cae403f6d42a88dc713352e5430fac
rifkiay <rifkiainul422@gmail.com>
2024-07-16 13:40:08 +0700

    fix env

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:GamePedia/homepage/appbardll.dart';
 import 'package:GamePedia/signin_up/signin.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
 
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
